eaa8a405f217ad9679d004886515685a4ec957e9
Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
2014-04-16 08:29:54 +0100

    Fixed control experiment

--- test_registers/time_registers.c
+++ test_registers/time_registers.c
@@ -9,6 +9,7 @@
 
 uint64_t rdtsc();
 
+//TODO check flags for formatting of printfs
 int main(int argc, const char ** argv) {
 	int k;
 
@@ -17,50 +18,74 @@ int main(int argc, const char ** argv) {
 	printf("Int\n");
 	for (k = 0; k < 10; ++k) {
 		register int x = 10;
+		int z = 10;
 		int y = 0;
 		unsigned long long time1, time2;
 		usleep(100); // To wait until the process is scheduled
 		time1 = rdtsc();
 		x = y;
 		time2 = rdtsc();
-		printf("%d. %llu %d\n", k, time2 - time1, x);
+		printf("R: %d. %llu %d\n", k, time2 - time1, x);
+
+		time1 = rdtsc();
+		x = z;
+		time2 = rdtsc();
+		printf("N: %d. %llu %d\n", k, time2 - time1, x);
 	}
 
 	printf("Double\n");
 	for (k = 0; k < 10; ++k) {
 		register double x = 10;
+		double z = 10;
 		double y = 0;
 		unsigned long long time1, time2;
 		usleep(100); // To wait until the process is scheduled
 		time1 = rdtsc();
 		x = y;
 		time2 = rdtsc();
-		printf("%d. %llu %f\n", k, time2 - time1, x);
+		printf("R: %d. %llu %f\n", k, time2 - time1, x);
+
+		time1 = rdtsc();
+		x = z;
+		time2 = rdtsc();
+		printf("N: %d. %llu %f\n", k, time2 - time1, x);
 	}
 
 	printf("Long\n");
 	for (k = 0; k < 10; ++k) {
 		register long x = 10l;
+		long z = 10l;
 		long y = 0l;
 		unsigned long long time1, time2;
 		usleep(100); // To wait until the process is scheduled
 		time1 = rdtsc();
 		x = y;
 		time2 = rdtsc();
-		printf("%d. %llu %lu\n", k, time2 - time1, x);
+		printf("R: %d. %llu %lu\n", k, time2 - time1, x);
+
+		time1 = rdtsc();
+		x = z;
+		time2 = rdtsc();
+		printf("N: %d. %llu %lu\n", k, time2 - time1, x);
 
 	}
 
 	printf("Long Long\n");
 	for (k = 0; k < 10; ++k) {
 		register long long x = 10ll;
+		long long z = 10ll;
 		long long y = 0ll;
 		unsigned long long time1, time2;
 		usleep(100); // To wait until the process is scheduled
 		time1 = rdtsc();
 		x = y;
 		time2 = rdtsc();
-		printf("%d. %llu %llu\n", k, time2 - time1, x);
+		printf("R: %d. %llu %llu\n", k, time2 - time1, x);
+
+		time1 = rdtsc();
+		x = z;
+		time2 = rdtsc();
+		printf("N: %d. %llu %llu\n", k, time2 - time1, x);
 	}
 }
 
